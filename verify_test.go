9f1c2bb93a84b9dad594bc3320d95ccffee9454f
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2017-02-03 11:05:17 +0100

    More tests

--- verify_test.go
+++ verify_test.go
@@ -22,6 +22,7 @@ import (
 	"flag"
 	"testing"
 	"time"
+	"io/ioutil"
 )
 
 var vomsPath = flag.String("vomspath", "test-samples/vomsdir", "VOMS directory")
@@ -54,15 +55,80 @@ func iniVerifyOptions(t *testing.T) VerifyOptions {
 	return VerifyOptions{
 		Roots:       roots,
 		VomsDir:     *vomsPath,
-		CurrentTime: time.Now(),
+		CurrentTime: time.Date(2016, 05, 18, 12, 37, 30, 0, gmt),
 	}
 }
 
+// Verify a normal valid RFC proxy
 func TestVerifyRFC(t *testing.T) {
 	options := iniVerifyOptions(t)
-	options.CurrentTime = time.Date(2016, 05, 18, 12, 37, 30, 0, gmt)
 
 	p := loadProxy("test-samples/RfcProxy.pem", t)
+	if e := p.Verify(options); e != nil {
+		t.Error(e)
+	}
+}
+
+// Verify an entity certificate (no proxy!)
+func TestVerifyEC(t *testing.T) {
+	options := iniVerifyOptions(t)
+
+	p := loadProxy("test-samples/EC.pem", t)
+	if e := p.Verify(options); e != nil {
+		t.Error(e)
+	}
+}
+
+// Verify a valid RFC proxy, but expired
+func TestVerifyExpired(t *testing.T) {
+	options := iniVerifyOptions(t)
+	options.CurrentTime = time.Date(2020, 01, 01, 01, 01, 01, 0, gmt)
+
+	p := loadProxy("test-samples/RfcProxy.pem", t)
+	if e := p.Verify(options); e == nil {
+		t.Error("Verification must fail")
+	} else {
+		t.Log(e)
+	}
+}
+
+// Load an RFC proxy with an incomplete chain
+func TestIncomplete(t *testing.T) {
+	content, e := ioutil.ReadFile("test-samples/BadIncomplete.pem")
+	if e != nil {
+		t.Fatal(e)
+	}
+	p := &X509Proxy{}
+	if e = p.Decode(content); e == nil {
+		t.Error("Loading must fail")
+	}
+}
+
+// Verify an RFC proxy, not having installed the ROOT CA, *but* the proxy contains the full chain
+// Must fail
+func TestVerifyBadCA(t *testing.T) {
+	var e error
+	options := iniVerifyOptions(t)
+	// We know we have no root ca here
+	options.Roots, e = LoadCAPath("test-samples/vomsdir", false)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	p := loadProxy("test-samples/BadFull.pem", t)
+	if e := p.Verify(options); e == nil {
+		t.Error("Verification must fail")
+	} else {
+		t.Log(e)
+	}
+}
+
+// Verify a proxy generated from a proxy
+func TestVerifyNested(t *testing.T) {
+	options := iniVerifyOptions(t)
+	options.CurrentTime = time.Date(2017, 02, 03, 10, 15, 00, 0, gmt)
+
+	p := loadProxy("test-samples/NestedProxy.pem", t)
 	if e := p.Verify(options); e != nil {
 		t.Error(e)
 	}
